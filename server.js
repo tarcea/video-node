9bdc98d6ef569d76f45b20c60600d0905778b7f6
tarcea <tarcea@gmail.com>
2021-09-28 22:09:17 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+// const socket = require('socket.io');
+const { v4: uuidV4 } = require('uuid');
+
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(roomId, userId);
+  })
+});
+
+server.listen(3000);
